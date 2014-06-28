023bef07dec545567c2b30f95016c5a6e5c76c89
Zamith <zamith.28@gmail.com>
2014-06-28 13:45:06 +0100

    First example using polymer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require platform/platform
 //= require jquery
 //= require jquery_ujs
 //= require jquery.circliful
